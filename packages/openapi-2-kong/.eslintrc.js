233a367166e9b6b5727384a9742b7551ec583bf5
zeteref <zeteref@gmail.com>
2024-09-20 17:22:36 +0200

    Eslint should ignore parameters begining with underscore

--- packages/openapi-2-kong/.eslintrc.js
+++ packages/openapi-2-kong/.eslintrc.js
@@ -8,5 +8,17 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': OFF(TYPESCRIPT_CONVERSION),
     '@typescript-eslint/no-empty-interface': OFF(TYPESCRIPT_CONVERSION),
     '@typescript-eslint/no-use-before-define': OFF(TYPESCRIPT_CONVERSION),
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'args': 'all',
+        'argsIgnorePattern': '^_',
+        'caughtErrors': 'all',
+        'caughtErrorsIgnorePattern': '^_',
+        'destructuredArrayIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'ignoreRestSiblings': true,
+      },
+    ],
   },
 };
